ecc775fcaa522d5f1d75fad7d33f03dbd2f654a1
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2024-02-11 16:12:43 -0500

    Refactor day 2

--- src/day02.jl
+++ src/day02.jl
@@ -5,7 +5,7 @@ using AdventOfCode2023
 """
 Load inputs and solve the [Day 2](https://adventofcode.com/2023/day/2) puzzle.
 """
-function run()::Tuple{Int64,Int64}
+function run()::Tuple{UInt,UInt}
     inputs::Vector{String} = split(AdventOfCode2023.data[2], '\n')
     return solve(inputs), solve(inputs, true)
 end
@@ -14,53 +14,34 @@ MAX_RED_CUBES = 12
 MAX_GREEN_CUBES = 13
 MAX_BLUE_CUBES = 14
 
-function solve(list::Array{String,1}, part_2=false)::Int64
+function solve(list::Vector{String}, part_2=false)::UInt
     games = map(parse_game, list)
 
-    sum = 0
     if part_2
-        for game in games
-            sum += power(get_minimum_cube_set(game))
-        end
+        return sum(game -> power(get_minimum_cube_set(game)), games)
     else
-        for game in games
-            add = true
-            for set in game.sets
-                if set.red > MAX_RED_CUBES || set.green > MAX_GREEN_CUBES || set.blue > MAX_BLUE_CUBES
-                    # Impossible state, exclude
-                    add = false
-                end
-            end
-
-            if add
-                sum += game.id
-            end
-        end
+        return sum(game -> game.id, filter(is_valid_game, games))
     end
-    return sum
 end
 
 "Set of cubes drawn"
 struct CubeSet
-    red::Int64
-    green::Int64
-    blue::Int64
+    red::UInt
+    green::UInt
+    blue::UInt
 end
 "Calculate the power of a CubeSet as defined in problem statement"
-function power(set::CubeSet)::Int64
+function power(set::CubeSet)::UInt
     return set.red * set.green * set.blue
 end
 
 "Game definiton"
 struct Game
-    id::Int64
+    id::UInt
     sets::Vector{CubeSet}
 end
 function get_minimum_cube_set(game::Game)::CubeSet
-    red = 0
-    green = 0
-    blue = 0
-
+    red, green, blue = 0, 0, 0
     for set in game.sets
         red = max(red, set.red)
         green = max(green, set.green)
@@ -72,12 +53,12 @@ end
 
 "Parse raw string data into Game struct"
 function parse_game(line::String)::Game
-    s = split(line, ":")
+    s = split(strip(line), ":")
     if length(s) != 2
         error("Invalid data format!")
     end
 
-    id = parse(Int64, replace(first(s), "Game " => ""))
+    id = parse(UInt, replace(strip(first(s)), "Game " => ""))
     sets = parse_cube_sets(last(s))
     return Game(id, sets)
 end
@@ -87,10 +68,8 @@ function parse_cube_sets(game_data::AbstractString)::Vector{CubeSet}
     sets::Vector{CubeSet} = []
 
     # CubeSet's within game_data are seperated by `;`
-    for set_data in split(game_data, ";")
-        red = 0
-        green = 0
-        blue = 0
+    for set_data in split(strip(game_data), ";")
+        red, green, blue = 0, 0, 0
 
         # Color entry within set_data are seperated by `,`
         for entry in split(strip(set_data), ",")
@@ -99,7 +78,7 @@ function parse_cube_sets(game_data::AbstractString)::Vector{CubeSet}
             if length(split_result) != 2
                 error("Invalid number and color format!")
             end
-            quantity = parse(Int64, first(split_result))
+            quantity = parse(UInt, first(split_result))
             color = last(split_result)
 
             if color == "red"
@@ -116,6 +95,16 @@ function parse_cube_sets(game_data::AbstractString)::Vector{CubeSet}
     end
 
     return sets
+end
+
+function is_valid_game(game::Game)::Bool
+    for set in game.sets
+        if set.red > MAX_RED_CUBES || set.green > MAX_GREEN_CUBES || set.blue > MAX_BLUE_CUBES
+            # Impossible state, exclude
+            return false
+        end
+    end
+    return true
 end
 
 end # module
